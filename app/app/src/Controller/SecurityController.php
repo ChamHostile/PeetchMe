0db20f23ac33bfcffecb9005304520a4b53d8824
chamhostile <hamza.aboudou@gmail.com>
2022-12-08 03:19:13 +0100

    update security controller information grabbing

--- app/app/src/Controller/SecurityController.php
+++ app/app/src/Controller/SecurityController.php
@@ -67,51 +67,50 @@ class SecurityController extends AbstractController
   {
     $jsonData = json_decode($request->getContent());
     $em = $doctrine->getManager();
-
- 
+      
     if (isset($jsonData->data)) {
       $currentUser = $this->userRepository->findOneBy(['id' => $jsonData->data->id]);
-      if (isset($jsonData->data->name) && isset($jsonData->firstname) && isset($jsonData->data->phone)) {
-        $currentUser->setNom($jsonData->data->name);
-        $currentUser->setPrenom($jsonData->data->firstname);
-        $currentUser->setTelephone($jsonData->data->phone);
+      if (isset($jsonData->data->data->name) && isset($jsonData->data->data->firstname) && isset($jsonData->data->data->phone)) {
+        $currentUser->setNom($jsonData->data->data->name);
+        $currentUser->setPrenom($jsonData->data->data->firstname);
+        $currentUser->setTelephone($jsonData->data->data->phone);
       }
   
       $address = $this->addressRepository->findOneBy(['user_id' => $currentUser->getId()]);
   
-      if (!$address && isset($jsonData->data->address)) {
+      if (!$address && isset($jsonData->data->data->address)) {
         $address = new Address();
         $address->setUserId($currentUser);
-        $address->setAddress($jsonData->data->address);
+        $address->setAddress($jsonData->data->data->address);
         $em->persist($address);
       }
     }
 
 
-    if (isset($currentUser) && $currentUser->getUserType() === 2 && isset($jsonData->project) && isset($jsonData->project->skill)) {
+    if (isset($currentUser) && $currentUser->getUserType() === 2 && isset($jsonData->data->project) && isset($jsonData->data->project->skill)) {
       $project = new Project();
       $project->setUserId($currentUser);
-      $project->setDescription($jsonData->project->description);
-      $project->setName($jsonData->project->name);
-      $project->setField($jsonData->project->field);
+      $project->setDescription($jsonData->data->project->description);
+      $project->setName($jsonData->data->project->name);
+      $project->setField($jsonData->data->project->field);
       $em->persist($project);
 
-      $skillWantEntry = $this->skillsRepository->findOneBy(['name' => $jsonData->project->skill]);
+      $skillWantEntry = $this->skillsRepository->findOneBy(['name' => $jsonData->data->project->skill]);
       if (!$skillWantEntry) {
         $skillWant = new Skills();
-        $skillWant->setName($jsonData->project->skill);
+        $skillWant->setName($jsonData->data->project->skill);
         $em->persist($skillWant);
       }
     }
 
-    if (isset($jsonData->data) && isset($currentUser) && 
-    $currentUser->getUserType() === 2 && isset($jsonData->data->skill) && isset($jsonData->data->skill1) && isset($jsonData->data->skill2) && isset($jsonData->data->skill3) && isset($jsonData->data->skill4)) {
+    if (isset($jsonData->data->data) && isset($currentUser) && 
+    $currentUser->getUserType() === 2 && isset($jsonData->data->data->skill) && isset($jsonData->data->data->skill1) && isset($jsonData->data->data->skill2) && isset($jsonData->data->data->skill3) && isset($jsonData->data->data->skill4)) {
 
       for ($i = 1; $i < 4 ; $i++) {
-        $skill = $this->skillsRepository->findOneBy(['name' => $jsonData['data']['skill'.$i]]);
+        $skill = $this->skillsRepository->findOneBy(['name' => $jsonData['data']['data']['skill'.$i]]);
         if (!$skill) {
           $skill = new Skills();
-          $skill->setName($jsonData['data']['skill'.$i]);
+          $skill->setName($jsonData['data']['data']['skill'.$i]);
           $em->persist($skill);
           if (isset($project)) $project->addSkill($skill);
         }
@@ -125,7 +124,8 @@ class SecurityController extends AbstractController
     if (!isset($currentUser)) $currentUser = "no user data to update";
 
     return $this->json([
-      'user'=> $currentUser
+      'user'=> $currentUser,
+      'data' => $jsonData
     ]);
   }
 
